586ffab6f281759152f4663c0a2d954bfc3ffd5d
Irina <petitpapillon82@gmail.com>
2022-09-17 21:45:55 +0400

    Add task 3 of HW

---
+++ task3_hw/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 68: Задайте значения M и N. 
+Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+M = 28; N = 7 -> 7 */
+
+Console.Clear();
+Console.WriteLine("Найдите наибольший общий делитель чисел M и N");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Наибольший общий делитель чисел {m} и {n} равен {nod(m, n)}");
+
+int nod(int m, int n)
+{
+    if (n == 0)
+        return m;
+    else
+        return nod(n, m % n);
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
