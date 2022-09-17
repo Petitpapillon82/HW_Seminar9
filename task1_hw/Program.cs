cdb28c203c0f78b675bad58d4c96db710df6155c
Irina <petitpapillon82@gmail.com>
2022-09-17 21:20:04 +0400

    Add task 1 of HW

---
+++ task1_hw/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"*/
+
+Console.Clear();
+Console.WriteLine("Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.");
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+Console.WriteLine(EvenNumbers(m, n));
+int EvenNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        if (m % 2 == 0 && m != 0)
+        {
+            Console.Write($"{m}, ");  
+        }
+        EvenNumbers(m + 1, n);
+	}
+    return 0;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
